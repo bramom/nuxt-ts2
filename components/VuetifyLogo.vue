08ac6665482c24dee5e90e8c21aa422e1e0725fc
Branimir <branimir.momcilovic@gmail.com>
2020-05-18 17:01:15 +0200

    Initial commit (Time: 5882 ms)

---
+++ components/VuetifyLogo.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="VueToNuxtLogo">
+    <div>Typescript component</div>
+    <div>
+      Name: {{ fullName }}
+      Message: {{ message }}
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script lang="ts">
+import Vue, { PropOptions } from 'vue'
+
+interface User {
+  firstName: string,
+  lastName: number
+}
+
+export default Vue.extend({
+  name: 'Logo',
+
+  props: {
+    user: {
+      type: Object,
+      required: true
+    } as PropOptions<User>
+  },
+
+  data () {
+    return {
+      message: 'This is a message'
+    }
+  },
+
+  computed: {
+    fullName (): string {
+      return `${this.user.firstName} ${this.user.lastName}`
+    }
+  }
+})
+</script>
